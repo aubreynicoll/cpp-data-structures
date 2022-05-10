3955a8237fc29666e6e629d187230a5c31a25ad9
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2022-05-10 18:49:45 +1000

    Add Methods for Vector Implementation
    Changes:
     - write methods for Vector implementation
     - write tests for each method

--- lib/Vector.h
+++ lib/Vector.h
@@ -1,30 +1,67 @@
 #pragma once
 
 #include <stddef.h>
+#include <string.h>
 
 #include <cstdlib>
+#include <stdexcept>
 
+/**
+ * @brief A basic Vector class. I've made an effort to remain faithful to the
+ * standard C++ Vector, but decided to scrap some things (e.g. push_front &
+ * pop_front, as array-style storage is not conducive to these operations)
+ *
+ * @tparam T
+ */
 template <class T>
 class Vector {
   T* _data;
   size_t _size;
   size_t _capacity;
 
+  /**
+   * @brief Get new memory allocation, copy data, free previous memory
+   * allocation
+   */
   void reallocate_storage(size_t);
+
+  /**
+   * @brief Doubles the vector's capacity and handles memory reallocation
+   */
   void increase_capacity();
+
+  /**
+   * @brief Halves the vector's capacity and handles memory reallocation
+   */
   void decrease_capacity();
 
  public:
   /**
-   * @brief Construct a new Vector object
-   * Default: size 0, capacity 0
+   * @brief Default Constructor: get Vector of size 0, capacity 0
    */
   Vector();
+
+  /**
+   * @brief Sized Constructor: get Vector  of size k, capacity k
+   *
+   * @param k size_t - sets size & capacity
+   */
   Vector(size_t);
+
+  /**
+   * @brief Fill Constructor: get Vector of size k, capacity k, filled with
+   * value
+   *
+   * @param k size_t - sets size & capacity
+   * @param val T - fill the array with val
+   */
   Vector(size_t, T);
+
+  /**
+   * @brief Destroy the Vector object
+   */
   ~Vector();
 
-  /* Capacity */
   /**
    * @brief Returns the current size of the Vector, which is equal
    * to the number of elements stored within.
@@ -41,34 +78,201 @@ class Vector {
    * @return size_t
    */
   size_t capacity();
-  void resize();
-  void reserve();
-  void shrink_to_fit();
-  bool empty();
 
-  /* Access */
+  /**
+   * @brief Resizes the vector to k elements. If k is less than size, any
+   * element of index >= k will be deleted. If k is greater than size, then new
+   * elements will be 0 initialized (as by calloc). If k is also greater than
+   * capacity, then capacity will be k.
+   *
+   * @param k size_t - The desired size
+   */
+  void resize(size_t);
+
+  /**
+   * @brief Resizes the vector to k elements. If k is less than size, any
+   * element of index >= k will be deleted. If k is greater than size, then new
+   * elements will be initialized to val. If k is also greater than
+   * capacity, then capacity will be k.
+   *
+   * @param k size_t - The desired size
+   * @param val T - New elements initialized to val
+   */
+  void resize(size_t, T);
+
+  /**
+   * @brief Increase the capacity of the vector to k. If k < capacity, this does
+   * nothing.
+   *
+   * @param k size_t - The new capacity, if k > capacity
+   */
+  void reserve(size_t);
+
+  /**
+   * @brief Shrink capacity to equal size
+   */
+  void shrink_to_fit();
+
+  /**
+   * @brief Returns true if the vector has 0 elements
+   *
+   * @return bool
+   */
+  bool is_empty();
+
+  /**
+   * @brief Get value at index
+   *
+   * @param index size_t
+   *
+   * @return T
+   * @throws out-of-bounds if index < 0 or index >= size
+   */
   T at(size_t);
+
+  /**
+   * @brief Returns a pointer to the vector's internal storage. Beware that if
+   * storage is reallocated, this pointer is pointing to garbage.
+   *
+   * @return T*
+   */
   T* data();
 
-  /* Modifiers */
+  /**
+   * @brief Store value at index, overwriting previous element
+   *
+   * @param index size_t
+   * @param value T
+   *
+   * @throws out-of-bounds if index < 0 or index >= size
+   */
   void assign(size_t, T);
+
+  /**
+   * @brief Insert value at index. All elements to the right of index (and
+   * including index) are shifted right by one.
+   *
+   * @param index size_t
+   * @param value T
+   *
+   * @throws out-of-bounds if index < 0 or index > size
+   */
   void insert(size_t, T);
+
+  /**
+   * @brief Remove value at index. All elements to the right of index are
+   * shifted left by one.
+   *
+   * @param index size_t
+   *
+   * @return T
+   *
+   * @throws out-of-bounds if index < 0 or index >= size
+   */
   T remove(size_t);
+
+  /**
+   * @brief Push a value into storage, inserting it at the end of the vector
+   *
+   * @param value T
+   */
   void push(T);
+
+  /**
+   * @brief Pop a value from storage, removing it from the end of the vector
+   *
+   * @return T
+   *
+   * @throws out-of-bounds if size == 0
+   */
   T pop();
 
-  /* Nice to haves */
-  size_t index_of(T);
+  /**
+   * @brief Returns the index of the first matching value as defined by the
+   * predicate f. Returns -1 if no matching value is found.
+   *
+   * @param f a function that consumes a single param of type T, and which
+   * returns a bool
+   * @return size_t
+   */
+  size_t index_of(bool (*f)(T));
+
+  /**
+   * @brief Reverses the vector's internal storage in place
+   */
   void reverse();
-  void swap(Vector<T>);
+
+  /**
+   * @brief Swap data with another Vector<T>
+   *
+   * @param v Vector<T>
+   */
+  void swap(Vector<T>&);
+
+  /**
+   * @brief set size & capacity of Vector to 0 and free all allocated storage
+   */
   void clear();
 };
+
+template <class T>
+void Vector<T>::reallocate_storage(size_t k) {
+  if (k == 0) {
+    _size = 0;
+    _capacity = 0;
+
+    free(_data);
+    _data = NULL;
+  } else {
+    if (k < _size) {
+      _size = k;
+    }
+    _capacity = k;
+
+    T* new_data = (T*)malloc(_capacity * sizeof(T));
+    memcpy(new_data, _data, _size * sizeof(T));
+
+    free(_data);
+    _data = new_data;
+  }
+}
+
+template <class T>
+void Vector<T>::increase_capacity() {
+  size_t new_capacity = _capacity == 0 ? 1 : _capacity * 2;
+  reallocate_storage(new_capacity);
+}
+
+template <class T>
+void Vector<T>::decrease_capacity() {
+  if (_capacity == 0) return;
+  size_t new_capacity = _capacity / 2;
+  reallocate_storage(new_capacity);
+}
 
 template <class T>
 Vector<T>::Vector() {
   _size = 0;
   _capacity = 0;
   _data = NULL;
+}
+
+template <class T>
+Vector<T>::Vector(size_t k) {
+  _size = k;
+  _capacity = k;
+  _data = (T*)calloc(k, sizeof(T));
+}
+
+template <class T>
+Vector<T>::Vector(size_t k, T val) {
+  _size = k;
+  _capacity = k;
+  _data = (T*)malloc(_capacity * sizeof(T));
+
+  for (size_t i = 0; i < _size; i++) {
+    *(_data + i) = val;
+  }
 }
 
 template <class T>
@@ -84,4 +288,142 @@ size_t Vector<T>::size() {
 template <class T>
 size_t Vector<T>::capacity() {
   return _capacity;
+}
+
+template <class T>
+void Vector<T>::resize(size_t k) {
+  if (k > _capacity) {
+    reallocate_storage(k);
+  }
+
+  if (k > _size) {
+    memset(_data + _size, 0, (k - _size) * sizeof(T));
+  }
+
+  _size = k;
+}
+
+template <class T>
+void Vector<T>::resize(size_t k, T val) {
+  if (k > _capacity) {
+    reallocate_storage(k);
+  }
+
+  if (k > _size) {
+    for (size_t i = _size; i < k; i++) {
+      *(_data + i) = val;
+    }
+  }
+
+  _size = k;
+}
+
+template <class T>
+void Vector<T>::reserve(size_t k) {
+  if (k > _capacity) reallocate_storage(k);
+}
+
+template <class T>
+void Vector<T>::shrink_to_fit() {
+  if (_capacity > _size) reallocate_storage(_size);
+}
+
+template <class T>
+bool Vector<T>::is_empty() {
+  return _size == 0;
+}
+
+template <class T>
+T Vector<T>::at(size_t i) {
+  if (i >= _size) throw std::invalid_argument("index out of bounds");
+  return *(_data + i);
+}
+
+template <class T>
+T* Vector<T>::data() {
+  return _data;
+}
+
+template <class T>
+void Vector<T>::assign(size_t i, T val) {
+  if (i >= _size) throw std::invalid_argument("index out of bounds");
+  *(_data + i) = val;
+}
+
+template <class T>
+void Vector<T>::insert(size_t i, T val) {
+  if (i > _size) throw std::invalid_argument("index out of bounds");
+
+  if (_size == _capacity) increase_capacity();
+
+  memmove(_data + i + 1, _data + i, (_size - i) * sizeof(T));
+  *(_data + i) = val;
+  _size++;
+}
+
+template <class T>
+T Vector<T>::remove(size_t i) {
+  if (i >= _size) throw std::invalid_argument("index out of bounds");
+
+  T val = *(_data + i);
+  memmove(_data + i, _data + i + 1, (_size - i - 1) * sizeof(T));
+  _size--;
+
+  return val;
+}
+
+template <class T>
+void Vector<T>::push(T val) {
+  insert(_size, val);
+}
+
+template <class T>
+T Vector<T>::pop() {
+  return remove(_size - 1);
+}
+
+template <class T>
+size_t Vector<T>::index_of(bool (*f)(T)) {
+  for (size_t i = 0; i < _size; i++) {
+    if ((*f)(*(_data + i))) {
+      return i;
+    }
+  }
+  return -1;
+}
+
+template <class T>
+void Vector<T>::reverse() {
+  int *i, *j, temp;
+  i = _data;
+  j = _data + _size - 1;
+
+  while (i < j) {
+    temp = *i;
+    *i = *j;
+    *j = temp;
+
+    i++;
+    j--;
+  }
+}
+
+template <class T>
+void Vector<T>::swap(Vector<T>& v) {
+  size_t temp_size = _size;
+  _size = v._size;
+  v._size = temp_size;
+
+  size_t temp_capacity = _capacity;
+  _capacity = v._capacity;
+  v._capacity = temp_capacity;
+
+  T* temp_data = _data;
+  _data = v._data;
+  v._data = temp_data;
+}
+
+template <class T>
+void Vector<T>::clear() {
+  reallocate_storage(0);
 }
--- lib/tests/Vector.test.cpp
+++ lib/tests/Vector.test.cpp
@@ -4,6 +4,266 @@
 
 TEST(VectorTest, BasicConstructor) {
   Vector<int> v;
+
   EXPECT_EQ(v.size(), 0);
   EXPECT_EQ(v.capacity(), 0);
+  EXPECT_TRUE(v.is_empty());
+}
+
+TEST(VectorTest, SizedConstructor) {
+  Vector<int> v(10);
+
+  EXPECT_EQ(v.size(), 10);
+  EXPECT_EQ(v.capacity(), 10);
+  EXPECT_FALSE(v.is_empty());
+
+  int *p = v.data();
+  for (size_t i = 0; i < v.size(); i++) {
+    EXPECT_EQ(*(p + i), 0);
+  }
+}
+
+TEST(VectorTest, FillConstructor) {
+  Vector<int> v(10, 99);
+
+  EXPECT_EQ(v.size(), 10);
+  EXPECT_EQ(v.capacity(), 10);
+  EXPECT_FALSE(v.is_empty());
+
+  int *p = v.data();
+  for (size_t i = 0; i < v.size(); i++) {
+    EXPECT_EQ(*(p + i), 99);
+  }
+}
+
+TEST(VectorTest, Clear) {
+  Vector<int> v(3, 0);
+
+  v.clear();
+
+  EXPECT_EQ(v.size(), 0);
+  EXPECT_EQ(v.capacity(), 0);
+  EXPECT_TRUE(v.data() == NULL);
+}
+
+TEST(VectorTest, Resize) {
+  Vector<int> v;
+  int *p;
+
+  // trigger storage reallocation
+  v.resize(10);
+  EXPECT_EQ(v.size(), 10);
+  EXPECT_EQ(v.capacity(), 10);
+
+  p = v.data();
+  for (size_t i = 0; i < v.size(); i++) {
+    EXPECT_EQ(*(p + i), 0);
+    *(p + i) = 1;  // should be erased when resizing up
+  }
+
+  // downsize shouldn't change capacity
+  v.resize(0);
+  EXPECT_EQ(v.size(), 0);
+  EXPECT_EQ(v.capacity(), 10);
+
+  // upsize less than capacity will only trigger zero-initialization
+  v.resize(5);
+  EXPECT_EQ(v.size(), 5);
+  EXPECT_EQ(v.capacity(), 10);
+
+  for (size_t i = 0; i < v.size(); i++) {
+    EXPECT_EQ(*(p + i), 0);  // 1's turned into 0's
+  }
+}
+
+TEST(VectorTest, FillResize) {
+  Vector<int> v;
+  int *p;
+
+  // trigger storage reallocation
+  v.resize(10, 99);
+  EXPECT_EQ(v.size(), 10);
+  EXPECT_EQ(v.capacity(), 10);
+
+  p = v.data();
+  for (size_t i = 0; i < v.size(); i++) {
+    EXPECT_EQ(*(p + i), 99);
+    *(p + i) = 1;  // should be erased when resizing up
+  }
+
+  // downsize shouldn't change capacity
+  v.resize(0);
+  EXPECT_EQ(v.size(), 0);
+  EXPECT_EQ(v.capacity(), 10);
+
+  // upsize less than capacity will only trigger value-initialization
+  v.resize(5, 22);
+  EXPECT_EQ(v.size(), 5);
+  EXPECT_EQ(v.capacity(), 10);
+
+  for (size_t i = 0; i < v.size(); i++) {
+    EXPECT_EQ(*(p + i), 22);
+  }
+}
+
+TEST(VectorTest, Reserve) {
+  Vector<int> v;
+
+  v.reserve(100);
+
+  EXPECT_EQ(v.size(), 0);
+  EXPECT_EQ(v.capacity(), 100);
+
+  v.reserve(50);
+
+  EXPECT_EQ(v.size(), 0);
+  EXPECT_EQ(v.capacity(), 100);
+}
+
+TEST(VectorTest, At) {
+  Vector<int> v(5, 99);
+  EXPECT_EQ(v.at(0), 99);
+  EXPECT_EQ(v.at(v.size() - 1), 99);
+
+  // expect out-of-bounds errors
+  EXPECT_ANY_THROW(v.at(-1));
+  EXPECT_ANY_THROW(v.at(v.size()));
+}
+
+TEST(VectorTest, Assign) {
+  Vector<int> v(2);
+  int *p;
+
+  v.assign(0, 99);
+  v.assign(1, 5);
+
+  p = v.data();
+
+  EXPECT_EQ(*p, 99);
+  EXPECT_EQ(*(p + 1), 5);
+
+  // expect out-of-bounds errors
+  EXPECT_ANY_THROW(v.assign(-1, 1));
+  EXPECT_ANY_THROW(v.assign(v.size(), 1));
+}
+
+TEST(VectorTest, Insert) {
+  Vector<int> v(3, 99);
+
+  EXPECT_EQ(v.capacity(), 3);
+
+  v.insert(v.size(), 101);
+  v.insert(0, 1234);
+
+  // Triggers reallocation if capacity is too little
+  EXPECT_EQ(v.capacity(), 6);
+
+  int *p = v.data();
+  EXPECT_EQ(*(p + 0), 1234);
+  EXPECT_EQ(*(p + 1), 99);
+  EXPECT_EQ(*(p + 2), 99);
+  EXPECT_EQ(*(p + 3), 99);
+  EXPECT_EQ(*(p + 4), 101);
+
+  // Expect out-of-bounds errors
+  EXPECT_ANY_THROW(v.insert(-1, 1));
+  EXPECT_ANY_THROW(v.insert(v.size() + 1, 1));
+}
+
+TEST(VectorTest, Remove) {
+  Vector<int> v(3);
+  int *p = v.data();
+
+  for (size_t i = 0; i < v.size(); i++) {
+    *(p + i) = i;
+  }
+
+  int a = v.remove(0);
+  int b = v.remove(v.size() - 1);
+
+  EXPECT_EQ(v.size(), 1);
+  EXPECT_EQ(v.capacity(), 3);
+  EXPECT_EQ(*p, 1);
+
+  // check that values are valid after memory is freed
+  v.clear();
+  EXPECT_EQ(a, 0);
+  EXPECT_EQ(b, 2);
+}
+
+TEST(VectorTest, Push) {
+  Vector<int> v;
+  for (size_t i = 0; i < 3; i++) {
+    v.push(i);
+  }
+
+  EXPECT_EQ(v.size(), 3);
+  EXPECT_EQ(v.capacity(), 4);
+
+  int *p = v.data();
+  for (size_t i = 0; i < 3; i++) {
+    EXPECT_EQ(*(p + i), i);
+  }
+}
+
+TEST(VectorTest, Pop) {
+  Vector<int> v(2, 99);
+
+  EXPECT_EQ(v.pop(), 99);
+  EXPECT_EQ(v.pop(), 99);
+
+  EXPECT_ANY_THROW(v.pop());
+
+  EXPECT_EQ(v.size(), 0);
+  EXPECT_EQ(v.capacity(), 2);
+}
+
+TEST(VectorTest, IndexOf) {
+  Vector<int> v(100);
+  int *p = v.data();
+  for (size_t i = 0; i < v.size(); i++) {
+    *(p + i) = i;
+  }
+
+  EXPECT_EQ(v.index_of([](int n) -> bool { return n == 5; }), 5);
+
+  EXPECT_EQ(v.index_of([](int n) -> bool { return n == -5; }), -1);
+}
+
+TEST(VectorTest, Reverse) {
+  Vector<int> v(100);
+  int *p;
+
+  p = v.data();
+  for (size_t i = 0; i < 100; i++) {
+    *(p + i) = i;
+  }
+
+  v.reverse();
+
+  for (size_t i = 0; i < 100; i++) {
+    EXPECT_EQ(*(p + i), 99 - i);
+  }
+}
+
+TEST(VectorTest, Swap) {
+  Vector<int> v1(5, 100);
+  Vector<int> v2(6, 20);
+  int *p;
+
+  v1.swap(v2);
+
+  EXPECT_EQ(v1.size(), 6);
+  EXPECT_EQ(v1.capacity(), 6);
+  p = v1.data();
+  for (size_t i = 0; i < v1.size(); i++) {
+    EXPECT_EQ(*(p + i), 20);
+  }
+
+  EXPECT_EQ(v2.size(), 5);
+  EXPECT_EQ(v2.capacity(), 5);
+  p = v2.data();
+  for (size_t i = 0; i < v2.size(); i++) {
+    EXPECT_EQ(*(p + i), 100);
+  }
 }
