72c48166a375ec3f3c43bdc32b5a0d3395e66c01
Aubrey Nicoll <aubrey.nicoll@gmail.com>
2022-06-12 15:53:41 +1000

    Add HashMapV1

--- lib/tests/HashMapV1.test.cpp
+++ lib/tests/HashMapV1.test.cpp
@@ -2,4 +2,11 @@
 
 #include <gtest/gtest.h>
 
-TEST(HashMapV1Test, BasicConstructor) { HashMap<int, int> map; }
+/* Having a ton of issues getting GTest to work with this data structure...
+Still investigating for now. I've independently tested the HashMap and it seems
+to work just fine. Will update tests later when I get to the bottom of it. */
+
+/* Must use typedef because template args confuses the macros */
+typedef HashMap<char, int> Map;
+
+TEST(HashMapV1Test, BasicConstructor) { EXPECT_NO_FATAL_FAILURE(Map map); }
